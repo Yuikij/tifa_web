21bf51761273069b1b91a00b1a9c1cb4b2e42c00
zxj <775718540@qq.com>
2020-06-17 15:36:07 +0800

    login

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,32 @@
 import styles from './index.css';
 import BasicDemo from '@/component/richText';
 import React from 'react';
+import {Button,Upload} from  'antd'
+import { UploadOutlined } from '@ant-design/icons';
 
 export default function() {
+  const props = {
+    name: 'file',
+    action: 'http://192.168.1.178:8081/inputImg',
+    headers: {
+      authorization: 'authorization-text',
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+      } else if (info.file.status === 'error') {
+      }
+    },
+  };
   return (
     <div className={styles.normal}>
+      <Upload {...props} >
+        <Button>
+          <UploadOutlined /> Click to Upload
+        </Button>
+      </Upload>
         <BasicDemo/>
     </div>
   );
