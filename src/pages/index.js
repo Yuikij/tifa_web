7106ad2c76bd437eaadc03424dbd9fb7320502d8
zxj <775718540@qq.com>
2020-07-01 16:57:10 +0800

    login

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import styles from './index.css';
 import BasicDemo from '@/component/richText';
-import React from 'react';
-import {Button,Upload} from  'antd'
+import React, { useState } from 'react';
+import { Button, Upload, Input } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import request from '@/utils/request';
 
 export default function() {
+  const [dotaId, setDotaId] = useState("");
+
   const props = {
     name: 'file',
     action: 'http://192.168.1.178:8081/inputImg',
@@ -20,38 +23,52 @@ export default function() {
       }
     },
   };
-  let socket = new WebSocket("ws://localhost:8081/websocket/123");
+  // let socket = new WebSocket('ws://localhost:8081/websocket/123');
   //打开事件
   //打开事件
-  socket.onopen = function() {
-    console.log("Socket 已打开");
-    socket.send("这是来自客户端的消息" + window.location.href + new Date());
-  };
-  //获得消息事件
-  socket.onmessage = function(msg) {
-    console.log(msg.data);
-    //发现消息进入    开始处理前端触发逻辑
-  };
-  //关闭事件
-  socket.onclose = function() {
-    console.log("Socket已关闭");
-  };
-  //发生了错误事件
-  socket.onerror = function() {
-    alert("Socket发生了错误");
-    //此时可以尝试刷新页面
-  }
+  // socket.onopen = function() {
+  //   console.log('Socket 已打开');
+  //   socket.send('这是来自客户端的消息' + window.location.href + new Date());
+  // };
+  // //获得消息事件
+  // socket.onmessage = function(msg) {
+  //   console.log(msg.data);
+  //   //发现消息进入    开始处理前端触发逻辑
+  // };
+  // //关闭事件
+  // socket.onclose = function() {
+  //   console.log('Socket已关闭');
+  // };
+  // //发生了错误事件
+  // socket.onerror = function() {
+  //   alert('Socket发生了错误');
+  //   //此时可以尝试刷新页面
+  // };
 
-
+  const clickId = () => {
+    let data = new FormData();
+    data.append('task', "share_like");
+    data.append('user_id', dotaId);
+    request("https://act.dota2.com.cn/heavenandhell", {
+      method: 'POST',
+      // headers: {
+      //   'token': token(),
+      // },
+      // headers: {'Content-Type': 'multipart/form-data'},
+      body: data,
+    });
+  };
 
   return (
     <div className={styles.normal}>
       <Upload {...props} >
         <Button>
-          <UploadOutlined /> Click to Upload
+          <UploadOutlined/> Click to Upload
         </Button>
       </Upload>
-        <BasicDemo/>
+      <Input onChange={(e)=>{
+        setDotaId(e.target.value)}} style={{width:"20%"}} addonBefore="id:" addonAfter={<Button onClick={clickId}>确定</Button>}/>
+      <BasicDemo/>
     </div>
   );
 }
